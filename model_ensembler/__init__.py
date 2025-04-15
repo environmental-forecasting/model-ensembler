c14b93a2bcd42df6784447f2915dc3853193ff37
thomaszwagerman <thozwa@bas.ac.uk>
2025-04-15 11:52:41 +0100

    propagate pyproject.toml metadata to init.py

--- model_ensembler/__init__.py
+++ model_ensembler/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for model_ensembler."""
+import importlib.metadata
 
-__author__ = """James Byrne"""
-__email__ = 'jambyr@bas.ac.uk'
-__version__ = '0.5.5'
+__author__ = importlib.metadata.authors.name("model_ensembler")
+__email__ = importlib.metadata.authors.email("model_ensembler")
+__version__ = importlib.metadata.version("model_ensembler")
